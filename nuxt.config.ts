ee6b5824252dfd7fceb18ef42f4f9aec99cee53e
Marek Schir <schir2@msn.com>
2025-03-27 23:54:53 -0400

    who knows

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+    modules: [
+        '@nuxtjs/supabase',
+        'shadcn-nuxt'
+    ],
+    shadcn: {
+        /**
+         * Prefix for all the imported component
+         */
+        prefix: '',
+        /**
+         * Directory that the component lives in.
+         * @default "./components/ui"
+         */
+        componentDir: './components/ui'
+    },
+    supabase: {
+
+        url: process.env.SUPABASE_URL,
+        key: process.env.SUPABASE_KEY,
+        serviceKey: process.env.SUPABASE_SERVICE_KEY,
+
+    },
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true}
 })
---
+++ pages/login.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+const email = ref('')
+const password = ref('')
+
+const signInWithOtp = async () => {
+  console.log('login')
+  const {error} = await supabase.auth.signInWithPassword({
+    email: email.value,
+    password: password.value
+  })
+  if (error) console.log(error)
+}
+</script>
+<template>
+  <div>
+    Username
+    <input v-model="email"/>{{email}}
+    Password?
+    <input v-model="password"/>{{password}}
+    <button @click="signInWithOtp">
+      Sign In with E-Mail
+    </button>
+  </div>
+</template>
